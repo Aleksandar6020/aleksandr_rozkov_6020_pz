f2c6d56bdf5f6105a4208e24897a56a30cca4508
Alexandr <rozhkov.a.ju@gmail.com>
2026-06-07 18:22:47 +0200

    Register API routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+const manulRoutes = require("./routes/manulRoutes");
+const suggestionRoutes = require("./routes/suggestionRoutes");
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/manuls", manulRoutes);
+app.use("/api/suggestions", suggestionRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
