01ae8ea659da71e1f4dd5d566d297092c5b3beae
Alexandr <rozhkov.a.ju@gmail.com>
2026-06-14 05:04:55 +0200

    Create authentication controller skeleton

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const register = (req, res) => {
+    res.json({ message: "Register endpoint" });
+};
+
+const login = (req, res) => {
+    res.json({ message: "Login endpoint" });
+};
+
+module.exports = {
+    register,
+    login
+};
